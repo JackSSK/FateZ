7e26b6b27efb980d307f2c0d17d0cc1dccfb7c80
jiang_junyao <jiangjunyao789@163.com>
2022-12-10 13:55:07 +0800

    Update preprocessor.py

--- fatez/process/preprocessor.py
+++ fatez/process/preprocessor.py
@@ -8,7 +8,7 @@ import pandas as pd
 import pyranges as pr
 import scanpy as sc
 import anndata as ad
-import numpy as np
+import fatez.tool.gff as gff
 
 class Process():
     """
@@ -19,12 +19,14 @@ class Process():
     def __int__(self,
         rna_path:str = None,
         atac_path:str = None,
+        gff_path:str = None
         ):
         self.rna_path = rna_path
         self.atac_path = atac_path
+        self.gff_path = gff_path
         self.rna_mt = list()
-        self.peak_dict = dict()
-        self.peak_pr = list()
+        self.peak_region_list = list()
+        self.gene_region_list = list()
 
     """
     :param rna_path: <class fatez.lib.preprocess.preprocess>
@@ -43,6 +45,25 @@ class Process():
             cache = True
         )
 
+        gff = gff.Reader(gff_path)
+        gff_template = gff.get_genes_gencode(id='GRCm38_template')
+
+        symbol_list = []
+        gene_chr_list = []
+        gene_start_list = []
+        gene_end_list = []
+        for i in list(mm10_template.genes.keys()):
+
+            symbol_list.append(gff_template.genes[i].symbol)
+            gene_chr_list.append(gff_template.genes[i].chr)
+            gene_start_list.append(gff_template.genes[i].start_pos)
+            gene_end_list.append(gff_template.genes[i].end_pos)
+            
+        if self.rna_mt[0][0:3] == 'ENS':
+            row_name_list = gff_template.genes.keys()
+        else:
+            row_name_list = symbol_list
+
         atac_h5ad = ad.read(self.atac_path)
 
         peak_names = list(atac_h5ad.var_names)
@@ -56,10 +77,6 @@ class Process():
             start_list.append(peak[1])
             end_list.append(peak[2])
 
-        ### create pyranges object
-        self.peak_dict = {
-            'Chromosome': chr_list,
-            'Start': start_list,
-            'End': end_list,
-        }
-        self.peak_pr = pr.from_dict(peak_dict)
+        self.peak_region_list = list(chr_list,start_list,end_list)
+        self.gene_region_list = list(gene_chr_list, gene_start_list, gene_end_list)
+
