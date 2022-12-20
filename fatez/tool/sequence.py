e288287e86bba082efb0c018adbe0da0d871c349
jiang_junyao <jiangjunyao789@163.com>
2022-12-21 00:56:43 +0800

    Update sequence.py

--- fatez/tool/sequence.py
+++ fatez/tool/sequence.py
@@ -4,11 +4,10 @@ from Bio import SeqIO
 
 class Reader(tool.Reader_Template):
     """
-    Load sequence file (fasta or fastq)
+    Load sequence file (fasta or anything else)
     """
     def get_fasta(self):
         fa_seq = {}
         for seq_record in SeqIO.parse(ref_seq_path, "fasta"):
             fa_seq[seq_record.id] = seq_record.seq
         return fa_seq
-        
