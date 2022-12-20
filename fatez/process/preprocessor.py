0122cad6287119128c01b1a21d3a52a46eb5b83c
jiang_junyao <jiangjunyao789@163.com>
2022-12-21 01:56:37 +0800

    Update preprocessor.py

--- fatez/process/preprocessor.py
+++ fatez/process/preprocessor.py
@@ -17,8 +17,8 @@ import pandas as pd
 from Bio import motifs
 import fatez.tool.gff as gff
 import fatez.tool.transfac as transfac
-
-class Preprocess():
+import fatez.tool.sequence as seq
+class Preprocesser():
     """
     Preprocessing the scRNA-seq and scATAC-seq data to get
     linkages between peaks and genes.
@@ -45,7 +45,12 @@ class Preprocess():
     :param atac_path: <class fatez.lib.preprocess.preprocess>
     	The object of prospective regulatory target gene.
     """
-    def load_data(self):
+
+
+
+    def load_data(self,rna_min_genes,rna_min_cells,rna_max_cells,rna_mt_counts):
+
+        ### load rna
         sc.settings.verbosity = 3
         sc.logging.print_header()
         sc.settings.set_figure_params(dpi=80, facecolor='white')
@@ -54,8 +59,16 @@ class Preprocess():
             var_names = 'gene_symbols',
             cache = True
         )
-        sc.pp.filter_cells(self.rna_mt, min_genes=200)
-        sc.pp.filter_genes(self.rna_mt, min_cells=3)
+
+        ### rna qc
+        sc.pp.filter_cells(self.rna_mt, min_genes=rna_min_genes)
+        sc.pp.filter_genes(self.rna_mt, min_cells=rna_min_cells)
+        self.rna_mt.var['mt'] = self.rna_mt.var_names.str.startswith('MT-')
+        sc.pp.calculate_qc_metrics(self.rna_mt, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+        self.rna_mt = self.rna_mt[self.rna_mt.obs.n_genes_by_counts < rna_max_cells, :]
+        self.rna_mt = self.rna_mt[self.rna_mt.obs.pct_counts_mt < rna_mt_counts, :]
+
+        ### load gff
         gff = gff.Reader(self.gff_path)
         gff_template = gff.get_genes_gencode(id='GRCm38_template')
 
@@ -77,6 +90,7 @@ class Preprocess():
 
         self.atac_h5ad = ad.read(self.atac_path)
 
+
         peak_names = list(self.atac_h5ad.var_names)
         chr_list = list()
         start_list = list()
@@ -91,6 +105,50 @@ class Preprocess():
         self.peak_region_df = pd.DataFrame({'chr':chr_list,'start':start_list,'end':end_list},index=peak_names)
         self.gene_region_df = pd.DataFrame({'chr':gene_chr_list, 'start':gene_start_list,
                                                                         'end':gene_end_list},index=row_name_list)
+
+    def merge_peak(self,width=250):
+        """
+        This function merge peaks and fix the peak length as 500bp,
+        which is easier for downstream analysis.
+        """
+        peak_df = self.gene_region_df
+        ### set query peak. The first peak is query peak
+        chr1 = peak_df.iloc[0,0]
+        start1 = peak_df.iloc[0,1]
+        end1 = peak_df.iloc[0,2]
+        ###
+        final_chr = []
+        final_start = []
+        final_end = []
+        for i in peak_df.index:
+            ### set target peak
+            chr2 = peak_df.loc[i,0]
+            start2 = peak_df.loc[i,1]
+            end2 = peak_df.loc[i,2]
+            ### merge peak
+            if chr1 == chr2 & ((start1>=start1 & start1<=end2) | (end1>=start2 & end1<=end2) |
+                               (start1<=start2 & end1 >end2) | (start2>end1 & (end2-start1) < width*2 ) |
+                               (start1>end2 & (end1-start2) < width*2 ) ):
+                new_region = sorted([start1,end1,start2,end2])
+                start1 = new_region[0]
+                end1 = new_region[3]
+            else:
+                ### fix peak region
+                new_start = start1 + int((end1-start1)/2) - width
+                new_end = start1 + int((end1-start1)/2) + width
+                if new_start<1:
+                    new_start = 1
+                    new_end = width*2
+                final_chr.append(chr1)
+                final_start.append(start1)
+                final_end.append(end1)
+                ### keep iterating
+                chr1 = chr2
+                start1 =start2
+                end1 = end2
+        self.gene_region_df = pd.DataFrame({'chr': final_chr, 'start': final_start,
+                                            'end': final_end}, index=row_name_list)
+
 
 
     def make_pseudo_networks(self,
@@ -121,7 +179,7 @@ class Preprocess():
 
 
     def find_linkages(self, overlap_size=250, cor_thr = 0.6):
-
+        
         ### find overlap
         gene_chr_type = list(set())
         gene_overlapped_peaks = {}
@@ -174,14 +232,19 @@ class Preprocess():
 
 
         ###
-    def find_motifs_binding(self, specie:str = 'mouse', region_use, gene,peak):
+    def find_motifs_binding(self, specie:str = 'mouse',ref_seq_path:str, peak_list:list):
+        """
+        This function calculate binding score for all motifs
+        in specific regions (overlap region between peak and
+        gene promoter).
+        """
         ### load tf motif relationships
         path = resource_filename(
             __name__, '../data/' + specie +'/Transfac201803_MotifTFsF.txt.gz'
         )
         ### make TFs motifs dict
-        tf_motifs = transfac.Reader(path = path).get_tfs()
-        motifs_use = tf_motifs[gene]['motif']
+        tf_motifs = transfac.Reader(path = path).get_motifs()
+        motifs_use = [tf_motifs.keys()]
 
         # motif_db = pd.read_table(path)
         # TF_motif_dict = {}
@@ -195,6 +258,9 @@ class Preprocess():
         #         else:
         #             TF_motif_dict[i] = [Motif]
 
+        ### load reference seq
+        ref_seq = seq.Reader(path=ref_seq_path).get_fasta()
+
         ### load TRANSFAC PWM
         pwm_path = resource_filename(
             __name__, '../data/' + specie +'/Transfac_PWM.txt'
@@ -203,16 +269,31 @@ class Preprocess():
         record = motifs.parse(handle, "TRANSFAC")
         handle.close()
         score_all = 0
+        peak_motif_dict = {}
         ### motif discovering
-        for i in motifs_use:
-            score_dict[i] = []
-            motif_use_name = i[1:5]
-            motif = record[int(motif_use_name)]
-            pwm = motif.counts.normalize()
-            pssm = pwm.log_odds()
-            for position, score in pssm.search(peak, threshold=3.0):
-                score_all += score
-        return [gene,score_all]
+        for peak in peak_list:
+            ### extract peak sequence
+            chr = peak.split('_')[0]
+            start = int(peak.split('_')[1])
+            end = int(peak.split('_')[2])
+            peak_sequence = ref_seq[chr][start-1:end]
+
+            motif_score_dict = {}
+            ### motif discovering
+            for i in motifs_use:
+                score_dict[i] = []
+                motif_use_name = i[1:5]
+                motif = record[int(motif_use_name)]
+                pwm = motif.counts.normalize()
+                pssm = pwm.log_odds()
+                for position, score in pssm.search(peak_sequence, threshold=0):
+                    motif_score_dict[i] = score
+            peak_motif_dict[peak] = motif_score_dict
+        return peak_motif_dict
+
+
+    def __get_tf_related_motif(self):
+
 
     def  __generate_grp(self):
         ### grp
