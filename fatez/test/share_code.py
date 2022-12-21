4f83be6af635414be69fc2b440a521409401ac77
JackSSK <angrayg@gmail.com>
2022-12-21 18:39:59 +0800

    v0.0.1a1
    here comes the example

--- fatez/test/share_code.py
+++ fatez/test/share_code.py
@@ -5,15 +5,11 @@ Blackboard to share codes
 import itertools
 
 
-list_1 = ['a', 'b', 'c','d']
-list_2 = [1,4,9]
+test_list = [1, 7, 4, 3]
 
+res = list(itertools.combinations(test_list, 2))
+print(res)
 
-unique_combinations = []
-
-for comb in itertools.permutations(list_1, len(list_2)):
-    zipped = zip(comb, list_2)
-    unique_combinations.append(list(zipped))
-
-
-print(unique_combinations)
+# iter mode
+for ele in itertools.combinations(test_list, 2):
+    print(ele)
