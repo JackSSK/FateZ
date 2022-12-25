7d75c9ad306f625454172b1802be40bd5fee202d
jiang_junyao <jiangjunyao789@163.com>
2022-12-26 01:42:42 +0800

    Update test_preprocess.py

--- fatez/test/test_preprocess.py
+++ fatez/test/test_preprocess.py
@@ -1,11 +1,29 @@
 import fatez.process.preprocessor as pre
 import os
-
+import fatez.tool.sequence as seq
 os.chdir("D:\\Westlake\\pwk lab\\fatez\\FateZ\\fatez\\test")
 if __name__ == '__main__':
-    peak_path = ('E:\\data_tutorial_buenrostro\\data_tutorial_buenrostro\\all_buenrostro_bulk_peaks.h5ad')
-    rna_path = ('D:\\Westlake\\pwk lab\\fatez\\hg19')
+    peak_path = ('../data/human/filtered_PBMC/')
+    rna_path = ('../data/human/filtered_PBMC/')
     gff_path = '../data/mouse/gencode.vM25.basic.annotation.gff3.gz'
-    network=pre.Preprocessor(rna_path,peak_path,gff_path)
+
+    ### load data
+    network = pre.Preprocessor(rna_path, peak_path, gff_path, data_type='paired')
     network.load_data()
     print(network.rna_mt)
+    print(network.atac_mt)
+    ### qc
+    network.rna_qc()
+    network.atac_qc()
+    print(network.rna_mt)
+    print(network.atac_mt)
+    ### merge & fix peak regions
+    atac_mt = network.merge_peak()
+    ### pseudo cell
+    ### load gene peak annotation
+    ### calculate correlation between gene and peak
+    ### select peak with top correlation
+    ### construct grp with expressed genes and its tfs with motif enrichment in target promoter
+
+
+
