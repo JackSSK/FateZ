52d9eb3fe02eddb47605a3f87ceb5a109024bbd6
jiang_junyao <jiangjunyao789@163.com>
2023-01-11 15:25:37 +0800

    Update model_testing.py

--- fatez/tool/model_testing.py
+++ fatez/tool/model_testing.py
@@ -13,5 +13,4 @@ def testing(dataloader, model, loss_fn,device):
 
     test_loss /= num_batches
     correct /= size
-    print(
-f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loss,correct
