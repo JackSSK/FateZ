2425b6f4b9ee2b471312a74020c625e9effded12
jiang_junyao <jiangjunyao789@163.com>
2023-01-11 23:22:27 +0800

    Update test_hsc_unpaired.py

--- fatez/test/test_hsc_unpaired.py
+++ fatez/test/test_hsc_unpaired.py
@@ -12,13 +12,17 @@ from torch.utils.data import DataLoader
 import fatez.lib as lib
 from fatez.tool import PreprocessIO
 import fatez.model.mlp as mlp
+from sklearn.model_selection import train_test_split
+import fatez.tool.JSON as JSON
+from fatez.tool import EarlyStopping
+from fatez.tool import model_testing
+import os
 """
 preprocess
 """
-device='cpu'
-#device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 ## preprocess parameters
-pseudo_cell_num_per_cell_type = 400
+pseudo_cell_num_per_cell_type = 2500
 correlation_thr_to_get_gene_related_peak = 0.4
 rowmean_thr_to_get_variable_gene = 0.1
 cluster_use =[1,4]
@@ -57,11 +61,18 @@ cluster_use =[1,4]
 # network.extract_motif_score(matrix2)
 # matrix2 = np.multiply(network.motif_enrich_score,matrix2)
 
+
 matrix1 = PreprocessIO.input_csv_dict_df(
     'D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_origi_label_mt/node/')
 matrix2 = pd.read_csv(
     'D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_origi_label_mt/edge_matrix.csv'
     ,index_col=0)
+
+# train_rate = 0.7
+# train_idx, val_test_idx, _, y_validate_test = train_test_split(index, labels, stratify=labels, train_size=train_rate,test_size=1-train_rate,
+#                                                  random_state=2, shuffle=True)
+# val_idx, test_idx, _, _ = train_test_split(val_test_idx,y_validate_test, train_size=val_rate/(1-train_rate), test_size=1-val_rate/(1-train_rate),
+#                                                      random_state=2, shuffle=True)
 
 
 ### samples and labels
@@ -72,18 +83,53 @@ for i in range(len(matrix1)):
     m2 = torch.from_numpy(matrix2.to_numpy())
     m1 = m1.to(torch.float32)
     m2 = m2.to(torch.float32)
+    # m1 = m1.to(device)
+    # m2 = m2.to(device)
     samples.append([m1, m2])
 labels = torch.from_numpy(np.repeat(range(len(cluster_use))
                                     ,len(matrix1)/len(cluster_use)))
 labels = labels.long()
+labels = labels.to(device)
+###
+"""
+hyperparameters
+"""
+batch_size = 20
+num_epoch = 200
+n_hidden = 4
+nhead = 4
+lr = 1e-4
+test_size = 0.3
+early_stop_tolerance = 15
+data_save = True
+data_save_dir = 'D:\\Westlake\\pwk lab\\fatez\\gat_gradient/nhead4_nhidden4/'
+outgat_dir = data_save_dir+'out_gat/'
+#os.makedirs(outgat_dir )
+"""
+dataloader
+"""
+X_train,X_test,y_train,y_test = train_test_split(
+    samples,labels,test_size=test_size,train_size = 1-test_size,random_state=0)
+train_dataloader = DataLoader(
+    lib.FateZ_Dataset(samples=X_train, labels=y_train),
+    batch_size=batch_size,
+    shuffle=True
+)
+
+test_dataloader = DataLoader(
+    lib.FateZ_Dataset(samples=X_test, labels=y_test),
+    batch_size=batch_size,
+    shuffle=True
+)
 """
 model define
 """
 model_gat = gat.GAT(
     d_model = 2,
     en_dim = 8,
-    nhead = None,
+    nhead = nhead,
     device = device,
+    n_hidden = n_hidden
 )
 decison = mlp.Classifier(
     d_model = 8,
@@ -101,16 +147,18 @@ bert_encoder = bert.Encoder(
 test_model = fine_tuner.Model(
     gat = model_gat,
     bin_pro = model.Binning_Process(n_bin = 100),
-    bert_model = bert.Fine_Tune_Model(bert_encoder, n_class = 2)
+    bert_model = bert.Fine_Tune_Model(bert_encoder, n_class = 2),
 )
 ### adam and CosineAnnealingWarmRestarts
 optimizer = torch.optim.Adam(test_model.parameters(),
-                             lr=0.0001,
+                             lr=lr,
                              weight_decay=1e-3)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,
                                                                  T_0=2,
                                                                  T_mult=2,
-                                                                 eta_min=0.0001/50)
+                                                                 eta_min=lr/50)
+
+early_stopping = EarlyStopping.EarlyStopping(tolerance=early_stop_tolerance, min_delta=10)
 model_gat.to(device)
 bert_encoder.to(device)
 test_model.to(device)
@@ -118,25 +166,20 @@ test_model.to(device)
 """
 traning
 """
-batch_size = 20
-num_epoch = 1500
-train_dataloader = DataLoader(
-    lib.FateZ_Dataset(samples=samples, labels=labels),
-    batch_size=batch_size,
-    shuffle=True
-)
+all_loss = list()
 for epoch in range(num_epoch):
     print(f"Epoch {epoch + 1}\n-------------------------------")
     batch_num = 1
     test_model.train()
+    train_loss = 0
+    train_acc = 0
     out_gat_data = list()
-    epoch_loss = 0
     for x,y in train_dataloader:
         optimizer.zero_grad()
         out_gat = model_gat(x[0], x[1])
         # torch.save(out_gat,
         #            'D:\\Westlake\\pwk lab\\fatez\\out_gat/epoch'+str(epoch)+'_batch'+str(batch_num)+'.pt')
-        output = test_model(x[0], x[1])
+        output = test_model(x[0].to(device), x[1].to(device))
         out_gat_data.append(out_gat.detach().tolist())
         loss = nn.CrossEntropyLoss()(
             output, y
@@ -144,13 +187,37 @@ for epoch in range(num_epoch):
         loss.backward()
         optimizer.step()
         acc = (output.argmax(1)==y).type(torch.float).sum()/batch_size
-        print(f"batch: {batch_num} loss: {loss} accuracy:{acc}")
+        #print(f"batch: {batch_num} loss: {loss} accuracy:{acc}")
         batch_num += 1
-        epoch_loss += loss
-    print(f"epoch: {epoch}, loss: {epoch_loss}")
+        train_loss += loss
+        train_acc += acc
+    print(
+     f"epoch: {epoch+1}, train_loss: {train_loss/batch_num}, test accuracy: {train_acc/batch_num}")
+    all_loss.append(train_loss)
     scheduler.step()
-    # JSON.encode(
-    #     out_gat_data,
-    #     'D:\\Westlake\\pwk lab\\fatez\\out_gat_hsc/epoch' + str(epoch) + '.pt'
-    # )
-model.Save(test_model.bert_model.encoder, '../data/ignore/unpaired_bert_encoder.model')
+    test_loss,test_acc = model_testing.testing(test_dataloader,
+                                               test_model, nn.CrossEntropyLoss()
+                                               , device=device)
+    print(
+        f"epoch: {epoch+1}, test_loss: {test_loss}, test accuracy: {test_acc}")
+    if data_save:
+        JSON.encode(
+            out_gat_data,
+            outgat_dir+str(epoch) + '.pt'
+        )
+    early_stopping(train_loss, test_loss)
+    if early_stopping.early_stop:
+        print("We are at epoch:", i)
+        break
+if data_save:
+    model.Save(test_model.bert_model.encoder,
+               data_save_dir+'bert_encoder.model')
+    # model.Save(test_model,
+    #            data_save_dir+'fine_tune.model')
+    model.Save(model_gat,
+               data_save_dir+'gat.model')
+
+"""
+testing
+"""
+
