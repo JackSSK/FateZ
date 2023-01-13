48d43d05a6cf2634a1ec1bdd2ef011b344562be4
jiang_junyao <jiangjunyao789@163.com>
2023-01-13 16:43:57 +0800

    Update test explain.py

--- fatez/test/test explain.py
+++ fatez/test/test explain.py
@@ -3,6 +3,7 @@ import fatez.model.mlp as mlp
 import torch
 import fatez.process.explainer as explainer
 import shap
+import fatez.model as model
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 mlp_param = {
@@ -13,10 +14,14 @@ mlp_param = {
     'dtype': torch.float32,
 }
 
-out_gat = JSON.decode('D:\\Westlake\\pwk lab\\fatez\\gat_gradient\\nhead0_nhidden1_weight_decay0\\out_gat\\99.pt')
+out_gat = JSON.decode('E:\\out_gat\\nhead0_nhidden1_weight_decay0/out_gat\\99.pt')
+model_gat = model.Load('E:\\out_gat\\nhead0_nhidden1_weight_decay0/gat.model')
 # u need to use trained model
 decision = mlp.Classifier(**mlp_param)
 epoch_out_gat=torch.tensor(out_gat[0])
+print(epoch_out_gat.shape)
+print(epoch_out_gat[0][1].shape)
+model_gat.explain(epoch_out_gat[0], epoch_out_gat[1])
 explain = shap.GradientExplainer(decision, epoch_out_gat)
 shap_values = explain.shap_values(out_gat)
 print(shap_values)
