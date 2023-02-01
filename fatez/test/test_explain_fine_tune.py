afea8c1651af469a6e3a778e27ae0a1ff938e9c2
jiang_junyao <jiangjunyao789@163.com>
2023-02-01 16:53:17 +0800

    111

--- fatez/test/test_explain_fine_tune.py
+++ fatez/test/test_explain_fine_tune.py
@@ -8,19 +8,13 @@ I suppose you want to explain BERT Fine Tune Model, not the decision model
 import fatez.lib as lib
 from torch.utils.data import DataLoader
 from fatez.tool import identify_regulons
-import fatez.tool.JSON as JSON
-import fatez.model.mlp as mlp
 import torch
-import fatez.process.explainer as explainer
 import fatez.process.fine_tuner as fine_tuner
-import fatez.model.bert as bert
 import fatez.model as model
-import shap
 from fatez.tool import PreprocessIO
 import pandas as pd
 import numpy as np
 import torch.nn as nn
-from fatez.tool import model_testing
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 mlp_param = {
     'd_model': 8,
@@ -30,29 +24,33 @@ mlp_param = {
     'dtype': torch.float32,
 }
 
-batch_size = 20
+batch_size = 2
+acc_all = 0
+explain_use = []
+explain_num = 1
+output_result = False
 fine_tuning = fine_tuner.Model(
-    gat = model.Load('D:\\Westlake\\pwk lab\\fatez\\gat_gradient\\nhead0_nhidden2_lr-3_epoch200_batch_size_20\\gat.model'),
+    gat = model.Load('D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_data_new_10000_02_5000_2500\\model\\nhead0_nhidden2_lr-3_epoch50_batch_size_20\\gat.model'),
     bin_pro = model.Binning_Process(n_bin = 100,config = None),
-    bert_model = model.Load('D:\\Westlake\\pwk lab\\fatez\\gat_gradient\\nhead0_nhidden2_lr-3_epoch200_batch_size_20\\bert_fine_tune.model')
+    bert_model = model.Load('D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_data_new_10000_02_5000_2500\\model\\nhead0_nhidden2_lr-3_epoch50_batch_size_20\\bert_fine_tune.model')
 )
 fine_tuning.to(device)
 matrix1 = PreprocessIO.input_csv_dict_df(
-    'D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_testing_data_10000/node/'
+    'D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_data_new_10000_02_5000_2500/node/'
 )
 matrix2 = pd.read_csv(
-    'D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_testing_data_10000/edge_matrix.csv',
+    'D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_data_new_10000_02_5000_2500/edge_matrix.csv',
     index_col = 0
 )
+m2 = torch.from_numpy(matrix2.to_numpy())
+m2 = m2.to(torch.float32)
 samples = []
 for i in range(len(matrix1)):
     m1 = matrix1[list(matrix1.keys())[i]]
     m1 = torch.from_numpy(m1.to_numpy())
-    m2 = torch.from_numpy(matrix2.to_numpy())
+
     m1 = m1.to(torch.float32)
-    m2 = m2.to(torch.float32)
-    # m1 = m1.to(device)
-    # m2 = m2.to(device)
+
     samples.append([m1, m2])
 labels = torch.from_numpy(np.repeat(range(2)
                                     ,len(matrix1)/2))
@@ -66,8 +64,6 @@ train_dataloader = DataLoader(
 )
 
 fine_tuning.eval()
-acc_all = 0
-explain_use = []
 with torch.no_grad():
     for x,y in train_dataloader:
         pred = fine_tuning(x[0].to(device), x[1].to(device))
@@ -76,11 +72,12 @@ with torch.no_grad():
         if correct == batch_size:
             explain_use.append(x)
         acc_all+=correct
+        if len(explain_use) == explain_num:
+            break
 print(acc_all)
 ### sum rank
-explain_use = explain_use[0:10]
 regulons = identify_regulons.Regulon(explain_use)
-regulons.explain_model(fine_tuning,batch_size,matrix2)
+regulons.explain_model(fine_tuning,batch_size,matrix2,filter_tf=False)
 regulon_count = regulons.sum_regulon_count()
 # regulon_count.index = list(matrix2.columns)
 # regulon_count = regulon_count[list(matrix2.index)]
@@ -89,7 +86,7 @@ print('---sum rank---')
 print(regulon_count)
 
 ### count top
-regulon_top = regulons.get_top_regulon_count(20)
+regulon_top = regulons.get_top_regulon_count(10)
 regulon_top = pd.Series(regulon_top)
 regulon_top = regulon_top.sort_values(ascending=False)
 print('---count top---')
@@ -98,15 +95,21 @@ print(regulons.tf_names[regulon_top.index])
 
 gene_name = pd.read_table('D:\\Westlake\\pwk lab\\HSC development\\data\\GSE137117\\rna_AE_Pre10x\\features.tsv.gz',header=None)
 gene_name.index = gene_name[0]
-sum_top = regulons.tf_names[regulon_count.index][0:20]
-count_top = regulons.tf_names[regulon_top.index][0:20]
+sum_top = regulons.tf_names[regulon_count.index]
+count_top = regulons.tf_names[regulon_top.index]
 
 print('sum rank top 20:')
 top_symbol = []
 for i in sum_top:
     symbol = gene_name[gene_name[0].isin([i])][1].to_numpy().tolist()[0]
     top_symbol.append(symbol)
-print(top_symbol)
+
+#print(top_symbol)
+if output_result:
+    with open('D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_data_new_10000_02_5000_2500\\model\\nhead0_nhidden2_lr-3_epoch50_batch_size_20\\sum_rank.txt','w+')as f:
+        for i in range(len(top_symbol)):
+            a1 = str(top_symbol[i])+'\n'
+            f.write(a1)
 
 
 print('count top top 20:')
@@ -115,4 +118,8 @@ for i in count_top:
     symbol = gene_name[gene_name[0].isin([i])][1].to_numpy().tolist()[0]
     top_symbol.append(symbol)
 print(top_symbol)
-
+if output_result:
+    with open('D:\\Westlake\\pwk lab\\fatez\\hsc_unpaired_data_new_10000_02_5000_2500\\model\\nhead0_nhidden2_lr-3_epoch50_batch_size_20\\count_top_10.txt','w+')as f:
+        for i in range(len(top_symbol)):
+            a1 = str(top_symbol[i])+'\n'
+            f.write(a1)
