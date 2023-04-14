2beddc925e571b0bee863f11705442dba94f41d8
jiang_junyao <jiangjunyao789@163.com>
2023-04-14 10:58:00 +0800

    Update scale_network.py

--- fatez/process/scale_network.py
+++ fatez/process/scale_network.py
@@ -1,10 +1,13 @@
-from sklearn.preprocessing import MinMaxScaler
+
 import numpy as np
 import pandas as pd
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
 def scale_network(network):
     network = network.replace(np.nan, 0)
     network = np.absolute(network)
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_network = scaler.fit_transform(np.asarray(network))
+    scaled_network = softmax(np.asarray(network))
     return scaled_network
