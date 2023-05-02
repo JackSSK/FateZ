985eebfcaa727f91aaafdda490cdab8fd292b090
JackSSK <angrayg@gmail.com>
2023-05-02 20:55:51 +0800

    v0.0.1a3M2
    Forgot moving test scripts of GAT to init

--- fatez/model/gnn/__init__.py
+++ fatez/model/gnn/__init__.py
@@ -52,3 +52,37 @@ def Set(config:dict=None, input_sizes:list=None, factory_kwargs:dict=None):
         return GATvD(**config['params'], **factory_kwargs)
     else:
         raise Error('Unknown GNN type')
+
+
+# if __name__ == '__main__':
+#     import fatez as fz
+#     from torch_geometric.datasets import TUDataset
+#     from torch_geometric.loader import DataLoader
+#     from torch_geometric.data import Data
+
+    # dataset = TUDataset(root='/tmp/MUTAG', name='MUTAG')
+    #
+    # test_dataset = dataset[10:20]
+    # train_loader = DataLoader(dataset[:10], batch_size = 2, shuffle = True)
+    # sample = dataset[0]
+    #
+    # gatmodel = Model(
+    #     d_model = 7, n_layer_set = 1, en_dim = 3, edge_dim = 4, device = 'cpu'
+    # )
+    #
+    # for step, data, in enumerate(train_loader):
+    #     result = gatmodel.model(data.x, data.edge_index, data.edge_attr)
+    #     break
+
+    # device = 'cpu'
+    # faker = fz.test.Faker(device = device).make_data_loader()
+    # gatmodel = Modelv2(
+    #     d_model = 2, n_layer_set = 1, en_dim = 3, edge_dim = 1, device = device
+    # )
+    # # Process data in GPU
+    # for x, y in faker:
+    #     result = gatmodel(x[0].to(device), x[1].to(device))
+    #     exp = gatmodel.explain(x[0][0].to(device), x[1][0].to(device))
+    #     break
+    # print(result.shape)
+    # print(exp)
