4c39dac08cdc110d2a02f7fa68d841faa2e41359
JackSSK <angrayg@gmail.com>
2023-06-08 21:05:23 +0800

    v0.0.1a5
    Need to revise on style

--- fatez/model/transformer/classifier.py
+++ fatez/model/transformer/classifier.py
@@ -4,6 +4,7 @@ Classifier for BERT-based model.
 
 author: jy, nkmtmsys
 """
+import torch
 import torch.nn as nn
 import fatez.model.transformer as transformer
 import fatez.model.position_embedder as pe
@@ -17,6 +18,7 @@ class Classifier(nn.Module):
     def __init__(self,
         rep_embedder = pe.Skip(),
         encoder:transformer.Encoder = None,
+        adaptor = 1,
         classifier = None,
         **kwargs
         ):
@@ -33,11 +35,43 @@ class Classifier(nn.Module):
         super(Classifier, self).__init__()
         self.rep_embedder = rep_embedder
         self.encoder = encoder
+        self.adaptor = adaptor
         self.classifier = classifier
 
 
-    def forward(self, input, mask = None):
-        output = self.rep_embedder(input)
-        output = self.encoder(output, mask)
+    def forward(self,
+            src: torch.Tensor,
+            mask: torch.Tensor = None,
+            src_key_padding_mask: torch.Tensor = None,
+            is_causal: bool = None,
+            ) -> torch.Tensor:
+        output = self.rep_embedder(src)
+        if self.adaptor is None:
+            output = self.encoder(output, mask, src_key_padding_mask, is_causal)
+        else:
+            output = self.deploy_adapter(
+                output, mask, src_key_padding_mask, is_causal
+            )
+
         output = self.classifier(output)
         return output
+
+    def deploy_adapter(self,
+            output: torch.Tensor,
+            mask: torch.Tensor = None,
+            src_key_padding_mask: torch.Tensor = None,
+            is_causal: bool = None,
+            ) -> torch.Tensor:
+        args, convert_to_nested = self.encoder.prepare(
+            output, mask, src_key_padding_mask, is_causal
+        )
+
+        for mod in self.encoder.encoder.layers:
+            mod.eval()
+            output = mod(**args)
+            mod.train()
+
+        if convert_to_nested:
+            output = output.to_padded_tensor(0.)
+
+        return self.encoder.normalize(output)
