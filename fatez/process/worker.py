8bf4e87ceae6df2daca57c3ff33603d0feb22e40
JackSSK <angrayg@gmail.com>
2023-06-29 22:54:33 +0800

    Update worker.py

--- fatez/process/worker.py
+++ fatez/process/worker.py
@@ -4,6 +4,7 @@ Setup worker envs for DistributedDataParallel objects.
 
 author: jy,
 """
+import torch
 import os
 import torch.distributed as dist
 
@@ -19,6 +20,9 @@ def setup(
     Init process with nccl for GPU training, or gloo for CPU training
     """
     if str(type(device)) == "<class 'list'>":
+        # Make sure using properly amount of device
+        device_count = torch.cuda.device_count()
+        assert device_count == len(device)
         os.environ['MASTER_ADDR'] = master_addr
         os.environ['MASTER_PORT'] = master_port
         dist.init_process_group(backend, rank=rank, world_size=len(device))
