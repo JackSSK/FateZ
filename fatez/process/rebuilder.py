db41611511f6d4ce0baa0a1fa0a04770edbe221a
jiang_junyao <jiangjunyao789@163.com>
2023-07-19 01:45:12 +0800

    Update rebuilder.py

--- fatez/process/rebuilder.py
+++ fatez/process/rebuilder.py
@@ -35,20 +35,21 @@ class rebuilder(object):
         self.result_dataloader = None
         self.predict_dataloader = None
         self.predict_true_dataloader = None
-        self.model = None
+        self.trainer = None
         self.train_batch_loss = []
         self.train_epoch_loss = []
         self.train_batch_cor = []
         self.train_epoch_cor = []
         self.predict_cor = []
+        self.predict_loss = []
 
-    def __tensor_cor_atac(self,tensor1, tensor2):
+    def __tensor_cor_atac(self,tensor1, tensor2,dim=0):
         all_cor = []
         for i in range(tensor1.shape[0]):
             tensor1_use = tensor1[i]
             tensor2_use = tensor2[i]
-            column1 = tensor1_use[:, 0].detach().numpy()
-            column2 = tensor2_use[:, 0].detach().numpy()
+            column1 = tensor1_use[:, dim].detach().numpy()
+            column2 = tensor2_use[:, dim].detach().numpy()
             correlation1 = np.corrcoef(column1, column2)[0, 1]
             all_cor.append(correlation1)
         return (np.array(all_cor).mean())
@@ -108,96 +109,120 @@ class rebuilder(object):
                         shape=self.matrix2[key_use].shape,
                     )
                 )
-        self.pertubation_dataloader = DataLoader(
+        pertubation_dataloader = DataLoader(
             lib.FateZ_Dataset(samples=samples1),
             batch_size=batch_size,
             collate_fn=lib.collate_fn,
             shuffle=False
         )
 
-        self.result_dataloader = DataLoader(
+        result_dataloader = DataLoader(
             lib.FateZ_Dataset(samples=samples2),
             batch_size=batch_size,
             collate_fn=lib.collate_fn,
             shuffle=False
         )
-        self.predict_dataloader = DataLoader(
+        predict_dataloader = DataLoader(
             lib.FateZ_Dataset(samples=samples3),
             batch_size=batch_size,
             collate_fn=lib.collate_fn,
             shuffle=False
         )
-        self.predict_true_dataloader = DataLoader(
+        predict_true_dataloader = DataLoader(
             lib.FateZ_Dataset(samples=samples4),
             batch_size=batch_size,
             collate_fn=lib.collate_fn,
             shuffle=False
         )
-    def train(self,config,prev_model_dir,device,epoch=5):
-        trainer = pre_trainer.Set(
-            config,
-            dtype=torch.float32,
-            device=device,
-            prev_model=model.Load(prev_model_dir)
-        )
-        trainer = pre_trainer.Set(
-            config,
-            node_recon_dim=1,
-            dtype=torch.float32,
-            device=device,
-            prev_model=trainer
-        )
+        return pertubation_dataloader,result_dataloader,predict_dataloader,predict_true_dataloader
+    def train(self,config,prev_model_dir = None,device = 'cuda',epoch=5,
+              pertubation_dataloader = None,
+              result_dataloader = None,
+              node_recon_dim = 2):
+        if prev_model_dir ==None:
+            self.trainer = pre_trainer.Set(
+                config,
+                node_recon_dim=node_recon_dim,
+                dtype=torch.float32,
+                device=device
+            )
+        else:
+            trainer = pre_trainer.Set(
+                config,
+                dtype=torch.float32,
+                device=device,
+                prev_model=model.Load(prev_model_dir)
+            )
+            self.trainer = pre_trainer.Set(
+                config,
+                node_recon_dim=2,
+                dtype=torch.float32,
+                device=device,
+                prev_model=trainer
+            )
         self.train_batch_loss = []
         self.train_epoch_loss = []
         self.train_batch_cor = []
         self.train_epoch_cor = []
-        trainer.setup()
-        size = trainer.input_sizes
+        self.trainer.setup()
+        size = self.trainer.input_sizes
         for i in range(epoch):
-            trainer.worker.train(True)
+            self.trainer.worker.train(True)
             best_loss = 99
             loss_all = 0
             cor_all = 0
             print('epoch-------' + str(i + 1))
-            for x, y in self.pertubation_dataloader:
+            for x, y in pertubation_dataloader:
 
                 # Prepare input data as always
-                input = [ele.to(trainer.device) for ele in x]
+                input = [ele.to(self.trainer.device) for ele in x]
 
-                node_rec, adj_rec = trainer.worker(input)
+                node_rec, adj_rec = self.trainer.worker(input)
 
                 # Prepare pertubation result data using a seperate dataloader
-                y = [self.result_dataloader.dataset.samples[ele].to(trainer.device)
+                y = [result_dataloader.dataset.samples[ele].to(self.trainer.device)
                      for ele in y]
 
-                node_results = torch.stack([ele.x for ele in y], 0)
+                node_results = torch.split(
+                    torch.stack([ele.x for ele in y], 0),
+                    node_rec.shape[1],
+                    dim=1
+                )[0]
 
                 """
                 Need to select training feature here by partioning node_results
                 """
 
                 # The input is not LogSoftmax-ed?
-                node_results = nn.LogSoftmax(dim=-2)(node_results)
-                node_results = node_results[:, :, 1]
-                node_results = node_results.reshape(node_results.shape[0], 1103,
-                                                    1)
+                if node_recon_dim == 1:
+                    node_results = nn.LogSoftmax(dim=-2)(node_results)
+                    node_results = node_results[:, :, 1]
+                    node_results = node_results.reshape(node_results.shape[0], 1103,
+                                                        1)
+                    cor_atac = self.__tensor_cor_atac(node_rec.cpu(),
+                                                      node_results.cpu(),dim=0)
+                elif node_recon_dim ==2:
+                    node_results = nn.LogSoftmax(dim=-2)(node_results)
+                    cor_atac = self.__tensor_cor_atac(node_rec.cpu(),
+                                                      node_results.cpu(),dim=1)
                 adj_results = lib.get_dense_adjs(
                     y, (size['n_reg'], size['n_node'], size['edge_attr'])
                 )
-                cor_atac = self.__tensor_cor_atac(node_rec.cpu(), node_results.cpu())
+
+
                 # cor_rna = tensor_cor_rna(node_rec.cpu(), node_results.cpu())
                 # all_cor_rna.append(cor_rna)
                 # Get total loss
-                loss = trainer.criterion(node_rec, node_results)
+                loss = self.trainer.criterion(node_rec, node_results)
                 if adj_rec is not None:
-                    loss += trainer.criterion(adj_rec, adj_results)
+                    loss += self.trainer.criterion(adj_rec, adj_results)
 
                 # Some backward stuffs here
                 loss.backward()
-                nn.utils.clip_grad_norm_(trainer.model.parameters(),
-                                         trainer.max_norm)
-                trainer.optimizer.step()
-                trainer.optimizer.zero_grad()
+                nn.utils.clip_grad_norm_(self.trainer.model.parameters(),
+                                         self.trainer.max_norm)
+                self.trainer.optimizer.step()
+                self.trainer.optimizer.zero_grad()
 
                 # Accumulate
                 best_loss = min(best_loss, loss.item())
@@ -207,31 +232,42 @@ class rebuilder(object):
                 # Some logs
                 self.train_batch_loss.append(float(loss.item()))
                 self.train_batch_cor.append(cor_atac)
-            self.model = trainer
-            self.train_epoch_loss.append(loss_all / len(self.pertubation_dataloader))
-            self.train_epoch_cor.append(cor_all / len(self.pertubation_dataloader))
 
-    def predict(self):
+            self.trainer.scheduler.step()
+            self.train_epoch_loss.append(loss_all / len(pertubation_dataloader))
+            self.train_epoch_cor.append(cor_all / len(pertubation_dataloader))
 
-        self.model.model.eval()
+    def predict(self,predict_dataloader,predict_true_dataloader,node_recon_dim = 2):
+
+        self.trainer.model.eval()
         self.predict_cor = []
         all_predict = []
         all_true = []
-        for x, y in self.predict_dataloader:
+        for x, y in predict_dataloader:
             # Prepare input data as always
-            input = [ele.to(self.model.device) for ele in x]
+            input = [ele.to(self.trainer.device) for ele in x]
             # Mute some debug outputs
-            node_rec, adj_rec = self.model.worker(input)
-            y = [self.predict_true_dataloader.dataset.samples[ele].to(self.model.device)
+            node_rec, adj_rec = self.trainer.worker(input)
+            y = [predict_true_dataloader.dataset.samples[ele].to(self.trainer.device)
                  for ele
                  in y]
             node_results = torch.stack([ele.x for ele in y], 0)
-            node_results = nn.LogSoftmax(dim=-2)(node_results)
-            node_results = node_results[:, :, 1]
-            node_results = node_results.reshape(node_results.shape[0], 1103, 1)
-            cor_atac = self.__tensor_cor_atac(node_rec.cpu(), node_results.cpu())
+            loss = self.trainer.criterion(node_rec, node_results)
+            if node_recon_dim == 1:
+                node_results = nn.LogSoftmax(dim=-2)(node_results)
+                node_results = node_results[:, :, 1]
+                node_results = node_results.reshape(node_results.shape[0], 1103,
+                                                    1)
+                cor_atac = self.__tensor_cor_atac(node_rec.cpu(),
+                                                  node_results.cpu(), dim=0)
+                node_rec = node_rec.reshape([node_rec.shape[0], 1103])
+            elif node_recon_dim == 2:
+                node_results = nn.LogSoftmax(dim=-2)(node_results)
+                cor_atac = self.__tensor_cor_atac(node_rec.cpu(),
+                                                  node_results.cpu(), dim=1)
+
             self.predict_cor.append(cor_atac)
-            node_rec = node_rec.reshape([node_rec.shape[0], 1103])
+            self.predict_loss.append(loss.item())
             all_predict.append(node_rec)
             all_true.append(node_results)
         return torch.cat(all_predict,dim=0),torch.cat(all_true,dim=0)
@@ -247,6 +283,6 @@ class rebuilder(object):
         epoch_report.to_csv(outputdir + prefix + '_epoch_report.csv')
         predict_report.to_csv(outputdir + prefix + '_predict_report.csv')
         model.Save(
-            self.model,
+            self.trainer,
             outputdir + prefix + '_model.model'
         )
