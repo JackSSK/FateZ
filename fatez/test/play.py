f54b03d1e463c256dc9ee1c5f0a96a240f56e518
JackSSK <angrayg@gmail.com>
2023-07-21 19:53:43 +0800

    v0.0.1a6-f0

--- fatez/test/play.py
+++ fatez/test/play.py
@@ -4,91 +4,72 @@ This is the playground for Multiomics rebuilder.
 
 author: jy
 """
-import os
-import pandas as pd
 import torch
-import copy
-import sys
 from torch.utils.data import DataLoader
-import torch_geometric.data as pyg_d
+import fatez.test.endo_hep_prepare as prep
 import fatez.lib as lib
-import fatez.model as model
-import fatez.tool.JSON as JSON
-import fatez.tool.PreprocessIO as PreprocessIO
-import fatez.process.early_stopper as es
-import fatez.process.fine_tuner as fine_tuner
-import fatez.process.pre_trainer as pre_trainer
-from fatez.process.scale_network import scale_network
-from sklearn.model_selection import train_test_split
-
-import tracemalloc
-import warnings
-from pkg_resources import resource_filename
-import shap
-import torch.nn as nn
-import fatez.model.mlp as mlp
-import fatez.model.gnn as gnn
-import fatez.model.criterion as crit
-import fatez.model.position_embedder as pe
 import fatez.process as process
 import fatez.process.worker as worker
+import fatez.process.fine_tuner as fine_tuner
+import fatez.process.pre_trainer as pre_trainer
 
 
 
-data_header = '../data/rd_fake_data/'
-node_dir = data_header+'all_node/'
-matrix1 = PreprocessIO.input_csv_dict_df(
-            node_dir,
-            df_type='node', order_cell=False)
-hep_edge = pd.read_table(data_header+'hep_edge.txt')
-endo_edge = pd.read_table(data_header+'endo_edge.txt')
-edge_dict = {'hep':hep_edge,'endo':endo_edge}
-for i in list(edge_dict.keys()):
-    edge = scale_network(edge_dict[i])
-    edge = torch.from_numpy(edge)
-    edge = edge.to(torch.float32)
-    edge_dict[i] = edge
-edge_label = pd.read_table(data_header+'fake_label.txt')
-edge_label.index = edge_label['sample']
-samples = []
-for i in range(len(matrix1)):
-    sample_name = list(matrix1.keys())[i]
-    m1 = torch.from_numpy(matrix1[sample_name].to_numpy()).to(torch.float32)
-    label = edge_label['label'][str(sample_name)]
-    if sample_name[0:3] == 'hep':
-        key_use = 'hep'
-    else:
-        key_use = 'endo'
-    inds, attrs = lib.get_sparse_coo(edge_dict[key_use])
-    samples.append(
-        pyg_d.Data(
-            x=m1,
-            edge_index=inds,
-            edge_attr=attrs,
-            y=label,
-            shape=edge_dict[key_use].shape,
+def test_full_model(
+    config:dict = None,
+    train_dataloader:DataLoader = None,
+    test_dataloader:DataLoader = None,
+    train_epoch:int = 20,
+    tune_epoch:int = 10,
+    quiet:bool = False,
+    device = 'cpu',
+    dtype = torch.float32,
+    ):
+    # Initialize
+    suppressor = process.Quiet_Mode()
+    worker.setup(device)
+
+    # Pre-train part
+    if quiet: suppressor.on()
+    trainer = pre_trainer.Set(config, device = device, dtype = dtype)
+    for i in range(train_epoch):
+        report = trainer.train(train_dataloader, report_batch = False,)
+        print(f'\tEpoch {i} Loss: {report.iloc[0,0]}')
+    if quiet: suppressor.off()
+
+    # Fine tune part
+    if quiet: suppressor.on()
+    tuner = fine_tuner.Set(config, trainer, device = device, dtype = dtype)
+    for i in range(tune_epoch):
+        report = tuner.train(train_dataloader, report_batch = False,)
+        print(f'\tEpoch {i} Loss: {report.iloc[0,0]}')
+    # Test fine tune model
+    report = tuner.test(test_dataloader, report_batch = True,)
+    print('Tuner Test Report')
+    print(report)
+    if quiet: suppressor.off()
+    print(f'\tFine-Tuner Green.\n')
+
+
+    # model.Save(trainer, 'faker_test.model')
+    # trainer = pre_trainer.Set(
+    #     config,
+    #     prev_model = model.Load('faker_test.model'),
+    #     **self.factory_kwargs
+    # )
+    # print('Save Load Green')
+    worker.cleanup(device)
+    return trainer
+
+
+
+
+if __name__ == '__main__':
+    device = 'cuda'
+    train_dataloader, test_dataloader = prep.get_dataloaders()
+    config = prep.get_config()
+    test_full_model(
+        config = config,
+        train_dataloader = train_dataloader,
+        test_dataloader = test_dataloader
         )
-    )
-
-
-"""
-preprocess
-"""
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# device = 'cpu'
-
-matrix1 = PreprocessIO.input_csv_dict_df(
-    data_header + 'endo_node',
-    df_type ='node',
-    order_cell = False,
-)
-
-print(matrix1)
-
-###load edge
-matrix2 = PreprocessIO.input_csv_dict_df(
-    'D:\\Westlake\\pwk lab\\fatez\\test_0613/edge/',
-    df_type ='edge',
-    order_cell = False,
-)
-gene_num = matrix2[list(matrix2.keys())[0]].columns
